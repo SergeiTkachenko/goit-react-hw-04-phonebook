834da35dc04b121657efef0296d0333071be3e39
SergeiTkachenko <fox.sergei92@gmail.com>
2023-03-10 23:27:47 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,48 @@
 import { Layout } from './Layout';
 import { GlobalStyle } from './GlobalStyle';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { AddForm } from './Form/Form';
 import { ContactList } from './ContactsList/ContactList';
 import { ContactFilter } from './ContactsFilter/ContactFilter';
 import { nanoid } from 'nanoid';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-
-  componentDidMount() {
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
     const savedContacts = localStorage.getItem('contacts');
     if (savedContacts !== null) {
       const parsedContacts = JSON.parse(savedContacts);
-      this.setState({ contacts: parsedContacts });
-      return;
+      return parsedContacts;
     }
-    this.setState({
-      contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      ],
-    });
-  }
+    return [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    ];
+  });
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  const [filter, setFilter] = useState('');
 
-  addContact = (newName, number) => {
-    const isNotUnique = this.state.contacts.some(
-      ({ name }) => name === newName
-    );
+  // componentDidMount() {
+  //   const savedContacts = localStorage.getItem('contacts');
+  //   if (savedContacts !== null) {
+  //     const parsedContacts = JSON.parse(savedContacts);
+  //     this.setState({ contacts: parsedContacts });
+  //     return;
+  //   }
+  //   this.setState({
+  //     contacts: [
+  //       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //     ],
+  //   });
+  // }
+
+  useEffect(
+    () => localStorage.setItem('contacts', JSON.stringify(contacts)),
+    [contacts]
+  );
+
+  const addContact = (newName, number) => {
+    const isNotUnique = contacts.some(({ name }) => name === newName);
     if (isNotUnique) {
       return alert(`${newName} is already in contacts.`);
     }
@@ -45,36 +51,34 @@ export class App extends Component {
       name: newName,
       number,
     };
-    this.setState(({ contacts }) => ({
-      contacts: [newContact, ...contacts],
-    }));
+    setContacts(contacts => [newContact, ...contacts]);
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  deleteContact = ContactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(item => item.id !== ContactId),
-      };
-    });
-  };
-
-  render() {
-    const visibleContacts = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+  const deleteContact = ContactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(item => item.id !== ContactId)
     );
+  };
+  // const deleteContact = contactId => {
+  //   setContacts(prevContacts =>
+  //     prevContacts.filter(contact => contact.id !== contactId)
+  //   );
+  // };
 
-    const { filter } = this.state;
-    return (
-      <Layout>
-        <AddForm onSubmit={this.addContact} />
-        <ContactFilter value={filter} onChange={this.changeFilter} />
-        <ContactList items={visibleContacts} onDelete={this.deleteContact} />
-        <GlobalStyle />
-      </Layout>
-    );
-  }
-}
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <Layout>
+      <AddForm onSubmit={addContact} />
+      <ContactFilter value={filter} onChange={changeFilter} />
+      <ContactList items={visibleContacts} onDelete={deleteContact} />
+      <GlobalStyle />
+    </Layout>
+  );
+};
